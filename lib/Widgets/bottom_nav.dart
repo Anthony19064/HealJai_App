3c7d7f81d9845a8fc689e592b26c960df0f1609a
Anthony19064 <habashieieina@gmail.com>
2025-07-24 20:54:41 +0700

    fix route

--- lib/Widgets/bottom_nav.dart
+++ lib/Widgets/bottom_nav.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:animate_do/animate_do.dart';
 
 
 import '../data_Lists/data_BottomBar.dart';
@@ -28,47 +29,50 @@ class _BottomNavState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     final navState = Provider.of<NavState>(context);
     
-    return Container(
-      color: const Color(0xFFFFF7EB),
+    return BounceInUp(
+      duration: Duration(milliseconds: 800),
       child: Container(
-        height: 60,
-        margin: const EdgeInsets.only(bottom: 35, left: 20, right: 20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Color(0xFFE0E0E0), width: 2),
-          borderRadius: BorderRadius.circular(15),
-        ),
-      
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children:
-              Bottom_data.asMap().entries.map((entry) {
-                int index = entry.key;
-                var item = entry.value;
-                bool isSelected = navState.selectedIndex == index;
-      
-                return GestureDetector(
-                  onTap: () {
-                    navState.setIndex(index);
-                    context.go(item['path']);
-                  },
-                  child: AnimatedContainer(
-                    duration: Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    width: 40,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      color:  isSelected? Color(0xFF78B465) : Color(0xFFFFFFFF),
-                      shape: BoxShape.circle,
+        color: const Color(0xFFFFF7EB),
+        child: Container(
+          height: 60,
+          margin: const EdgeInsets.only(bottom: 35, left: 20, right: 20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Color(0xFFE0E0E0), width: 2),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children:
+                Bottom_data.asMap().entries.map((entry) {
+                  int index = entry.key;
+                  var item = entry.value;
+                  bool isSelected = navState.selectedIndex == index;
+        
+                  return GestureDetector(
+                    onTap: () {
+                      navState.setIndex(index);
+                      context.go(item['path']);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color:  isSelected? Color(0xFF78B465) : Color(0xFFFFFFFF),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset(
+                        item['icon'],
+                        color: isSelected? Color(0xFFFFFFFF) : Color(0xFF8D8D8D),
+                        fit: BoxFit.scaleDown,
+                      ),
                     ),
-                    child: SvgPicture.asset(
-                      item['icon'],
-                      color: isSelected? Color(0xFFFFFFFF) : Color(0xFF8D8D8D),
-                      fit: BoxFit.scaleDown,
-                    ),
-                  ),
-                );
-              }).toList(),
+                  );
+                }).toList(),
+          ),
         ),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:provider/provider.dart';
+import 'package:animate_do/animate_do.dart';
 
-import '../Widgets/header_section.dart';
 import '../providers/userProvider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,20 +13,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   void initState() {
     super.initState();
   }
-
 
   Widget build(BuildContext context) {
     final userInfo = Provider.of<UserInfo>(context);
 
     return Column(
       children: [
-        Text(userInfo.userName != null? "Welcome na kub ${userInfo.userName}" : "D kub"),
-        ],
+        ZoomIn(
+          duration: Duration(milliseconds: 500),
+          child: Text(
+            userInfo.userName != null
+                ? "Welcome na kub ${userInfo.userName}"
+                : "D kub",
+          ),
+        ),
+      ],
     );
   }
 }
